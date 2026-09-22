1eeff436275c4f5b26b8f66e0b7ac1531219fa6a
Majid Ramezani <38846718+majidrgold@users.noreply.github.com>
2026-09-22 14:51:09 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # # 1. 
+        # from collections import defaultdict
+
+        # res = defaultdict(list)
+
+        # for s in strs:
+        #     key = ''.join(sorted(s))
+        #     res[key].append(s)
+        
+        # return list(res.values())
+        # # O(m * nlogn) o(m * n)
+        # 2. using key
+        from collections import defaultdict
+
+        res = defaultdict(list)
+
+        def get_key(s):
+            key = [0] * 26
+            for c in s:
+                key[ord(c) - ord('a')] += 1
+            return tuple(key)
+
+        for s in strs:
+            key = get_key(s)
+            res[key].append(s)
+        
+        return list(res.values())
+        # O(m * n), O(m * n) m: number of strings in strs, n: length of longest string
+
