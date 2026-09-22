6500310918da90d84d5f0f8e29f6f00f894a4133
Majid Ramezani <38846718+majidrgold@users.noreply.github.com>
2026-09-22 09:01:05 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force Solution would be O(n2) to search all pairs in nums
+        # We can use hash map for this problem
+        # in can be done in two path, one path we can go through it
+        # and get index of each value 
+        # second path we get position,
+        # we can also do it in one path
+        
+        counts = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            counts[complement] = i
+        
+        for i, num in enumerate(nums):
+            if num in counts and counts[num] != i:
+                return [i, counts[num]]
+
+        return -1
+
+        # # one path
+        # counts = {}
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+        #     if num in counts:
+        #         return [counts[nums], i]
+            
+        #     counts[complement] = i
+
+        # return -1
+            
+
+        
